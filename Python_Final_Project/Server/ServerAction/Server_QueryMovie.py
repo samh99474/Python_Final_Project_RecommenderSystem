49ff88e507eade13da02557ab518efbcbc0b6147
samh99474 <samh99474@gmail.com>
2021-06-12 12:10:19 +0800

    samh_QueryMovie v2

--- Python_Final_Project/Server/ServerAction/Server_QueryMovie.py
+++ Python_Final_Project/Server/ServerAction/Server_QueryMovie.py
@@ -19,17 +19,27 @@ class Server_QueryMovie:
                     MovieData_Dict["movieId"] = info["movieId"]"""
 
                 movieName = MovieData_Table().select_a_Movie_Name(info["id"])
+                movie_movieId = MovieID_map_Table().select_a_movieId(info["id"])
                 Movie_VoteAverage = MovieData_Table().select_a_Movie_VoteAverage(info["id"])
+                Movie_VoteCount = MovieData_Table().select_a_Movie_VoteCount(info["id"])
                 Movie_Overview = MovieData_Table().select_a_Movie_Overview(info["id"])
                 Movie_Genres = MovieData_Table().select_a_Movie_Genres(info["id"])
-                movie_movieId = MovieID_map_Table().select_a_movieId(info["id"])
+                Movie_release_date = MovieData_Table().select_a_Movie_release_date(info["id"])
+                Movie_Cast = MovieData_Table().select_a_Movie_Cast(info["id"])
+                Movie_Crew = MovieData_Table().select_a_Movie_Crew(info["id"])
                 
                 Movie_dict["id"] = info["id"] # Movie's id (from MovieData_Table)
+                Movie_dict["movieId"] = movie_movieId[0]# Movie's movieId (from Ratings_Table、MovieID_map_Table)
                 Movie_dict["movieName"] = movieName[0]
                 Movie_dict["vote_average"] = Movie_VoteAverage[0]
+                Movie_dict["vote_count"] = Movie_VoteCount[0]
                 Movie_dict["overview"] = Movie_Overview[0]
                 Movie_dict["genres"] = Movie_Genres[0]
-                Movie_dict["movieId"] = movie_movieId[0]# Movie's movieId (from Ratings_Table、MovieID_map_Table)
+                Movie_dict["release_date"] = Movie_release_date[0]
+                Movie_dict["cast"] = Movie_Cast[0]
+                Movie_dict["crew"] = Movie_Crew[0]
+                
+                
 
                 movie_list.append(Movie_dict)
                 status = True
